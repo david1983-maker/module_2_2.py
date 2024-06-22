defa28a9e3171abbeb43b42c6b9c01bbd7ce4c1f
david1983-maker <davidkilidzan94127@gmail.com>
2024-06-22 20:54:51 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,17 @@
+# Задача "Все ли равны"
+
+first=10
+second=6
+third=10
+
+if first==second==third:
+    print(3)
+     
+elif first==second or second==third or first==third:
+    print(2) 
+    
+else:
+    print(0) 
+     
+    
+   
